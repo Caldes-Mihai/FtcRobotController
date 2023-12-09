c806d86e4bf0cfba14346437ef42844d58ee8751
Caldes-Mihai <148072368+Caldes-Mihai@users.noreply.github.com>
2023-12-09 10:17:36 +0200

    custom PIDF Controller

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomPIDFController.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CustomPIDFController.java
@@ -2,26 +2,26 @@ package org.firstinspires.ftc.teamcode;
 
 import com.arcrobotics.ftclib.controller.PIDController;
 
-public class PIDFControllerUtility {
+public class CustomPIDFController extends PIDController {
     private double p;
     private double i;
     private double d;
     private double f;
     private double ticksToDeg;
-    private PIDController controller;
 
-    public PIDFControllerUtility(double p, double i, double d, double f, double rpm) {
+    public CustomPIDFController(double p, double i, double d, double f, double rpm) {
+        super(p, i, d);
         this.p = p;
         this.i = i;
         this.d = d;
         this.f = f;
         this.ticksToDeg = rpm / 180;
-        controller = new PIDController(p, i, d);
     }
 
+    @Override
     public double calculate(double ref, double target) {
-        controller.setPID(p, i, d);
-        double pid = controller.calculate(ref, target);
+        this.setPID(p, i, d);
+        double pid = this.calculate(ref, target);
         double ff = Math.cos(Math.toRadians(target / ticksToDeg)) * f;
         return pid + ff;
     }
